fdefbe891815e8af75993574a83f123b373a4ee3
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-08 21:36:21 +0100

    Dodavanje klasa i njihovih atributa, implementacija konstruktora

--- src/ba/unsa/etf/rpr/t1z4/Student.java
+++ src/ba/unsa/etf/rpr/t1z4/Student.java
@@ -5,7 +5,7 @@ public class Student {
     private String prezime;
     private int broj_indexa;
 
-    public Student(String ime, Student prezime, int broj_indexa) {
+    public Student(String ime, String prezime, int broj_indexa) {
         this.ime = ime;
         this.prezime = prezime;
         this.broj_indexa = broj_indexa;
